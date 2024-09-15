09cce2865600da316b1a0df8452d67a949c27ef8
Mikhail Kushnir <ro5ti5lav@yandex.ru>
2024-09-15 12:33:42 +0400

    Updates

---
+++ _next/static/kGczvfI0WR5Nqo7GeYzGV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/606-5d63d6339201b83b.js","static/chunks/pages/index-685fc5dfc465938c.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
