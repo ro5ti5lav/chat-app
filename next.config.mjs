2a6126ee29851fd766f867646ee83c7df05ee095
Mikhail Kushnir <ro5ti5lav@yandex.ru>
2024-09-15 13:23:50 +0400

    12

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Если ваш проект находится в корне репозитория, оставьте пустым
+    output: 'export',
     basePath: '/chat-app',
     assetPrefix: '/chat-app/', // Используется для корректного размещения ресурсов на GitHub Pages
     trailingSlash: true, // Добавляет косую черту в конце всех URL
